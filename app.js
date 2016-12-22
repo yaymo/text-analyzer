e89084e9540a2a30d1d894f681ee86b30a9a4f47
Jameson Hill <stevenjamesonhill@gmail.com>
2016-12-22 09:54:28 -0500

    fixed dumb mistake

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-function averageSentenceLength(text){
-  var totalSentences = text.match(/[.!?]+/g) ? text.match(/[.!?]+/g).length : 1;
+function averageWordsPerSentence(text) {
+  var numberSentences = text.match(/[.!?]+/g) ? text.match(/[.!?]+/g).length : 1;
   var wordCount = tokenizeText(text).length;
-  return (wordCount / totalSentences);
+  return (wordCount / numberSentences).toFixed(2);
 }
 
-function averageWordLength(tokens){
+function averageWordLength(tokens) {
   var totalLength = tokens.join("").length;
-  return (totalLength / tokens.length);
+  return (totalLength / tokens.length).toFixed(2);
 }
 
-function distinctWords(tokens){
+function numberDistinctWords(tokens) {
   var result = [];
   for (var i=0; i<tokens.length; i++){
-    if (result.indexOf(tokens[i]) === -1){
+    if (result.indexOf(tokens[i]) === -1) {
       result.push(tokens[i]);
     }
   }
@@ -22,34 +22,36 @@ function distinctWords(tokens){
 
 // .match(regex) returns a word once it finds at least one space at the beginning and end??
 // or... once we find a space at beginning and end, that's a word??
-function tokenizeText(text){
+function tokenizeText(text) {
   return text.toLowerCase().match(/\b[^\s]+\b/g).sort();
 }
 
 
 // find returns and optional new lines or just returns and replace with nothing??
-function removeSpaces(text){
+function removeSpaces(text) {
   return text.replace(/\r?\n|\r/g, "");
 }
 
 
-function textData(text){
+function textData(text) {
   var tokens = tokenizeText(text);
-  var uniqueWords = distinctWords(tokens);
+  var uniqueWords = numberDistinctWords(tokens);
   var totalWords = tokens.length;
   var wordLength = averageWordLength(tokens);
-  var sentenceLength = averageSentenceLength(text);
+  var sentenceLength = averageWordsPerSentence(text);
 
   var wordReport = $('.js-text-report');
   wordReport.find('.js-word-count').text(totalWords);
   wordReport.find('.js-word-unique').text(uniqueWords);
-  wordReport.find('.js-word-average').text(wordLength + " characters");
-  wordReport.find('.js-word-length').text(sentenceLength + " words");
+  wordReport.find('.js-word-average').text(
+    wordLength + " characters");
+  wordReport.find('.js-word-length').text(
+    sentenceLength + " words");
   wordReport.removeClass('hidden');
 }
 
-function handleSubmit(){
-  $('js-text-form').submit(function(event){
+function handleSubmit() {
+  $('.js-text-form').submit(function(event){
     event.preventDefault();
     var textEntry = $(this).find('#user-text').val();
     textData(removeSpaces(textEntry));
